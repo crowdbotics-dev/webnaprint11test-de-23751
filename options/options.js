3548ae4b41673d6e95cf98d80729c7f0b3ecf466
Crowdbotics <engineering@crowdbotics.com>
2022-03-29 09:38:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "webnaprint11test_de_23751",
+  url: "https://webnaprint11test_de_23751.botics.co",
+  api: "https://webnaprint11test_de_23751.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
